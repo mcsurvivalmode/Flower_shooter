6151d2a3edc18525d1f74b41f0909141a75c488e
mcsurvivalmode <C24385051@mytudublin.ie>
2025-04-02 15:29:30 +0100

    bullet moving sideways!

--- musical_shooter/water.gd
+++ musical_shooter/water.gd
@@ -1,7 +1,14 @@
 extends CharacterBody2D
 
+var speed = 400
 
-var speed = 300 
+func _process(delta):
+  move_local_x(speed * delta)
 
-func _physics_process(delta):
-	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
+
+
+
+#var speed = 300 
+
+#func _physics_process(delta):
+	#var collision_info = move_and_collide(velocity.normalized() * delta * speed)
