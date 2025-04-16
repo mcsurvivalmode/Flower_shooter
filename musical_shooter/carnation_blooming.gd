10b2f917fbdf08f4c084aae753310d3815056ffc
mcsurvivalmode <C24385051@mytudublin.ie>
2025-04-16 15:13:03 +0100

    blooming carnation scene added

---
+++ musical_shooter/carnation_blooming.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var animated_lily = $AnimatedSprite2D
+@onready var sparkly_sound = $SparklySound
+
+
+func _ready() -> void:
+	animated_lily.play("lily_bloom")
+	sparkly_sound.play()
