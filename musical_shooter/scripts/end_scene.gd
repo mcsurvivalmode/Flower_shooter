a538c00b283568161ad1a6f03ade5c96793688dd
mcsurvivalmode <C24385051@mytudublin.ie>
2025-04-18 01:59:18 +0100

    soooo annoying

--- musical_shooter/scripts/end_scene.gd
+++ musical_shooter/scripts/end_scene.gd
@@ -1,5 +1,5 @@
 extends Node2D
-@onready var scene = "res://scenes/garden_game.tscn"
+#@onready var scene = "res://scenes/garden_game.tscn"
 
 
 
@@ -7,3 +7,7 @@ extends Node2D
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.keycode == KEY_ESCAPE:
 		get_tree().quit()	
+
+#func _process(delta):
+#	$Label.text="Flowers Bloomed: " + str(scene.flower_count)
+	
